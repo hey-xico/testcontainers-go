b47775360d75e061771afb38f4c253866e4f7e43
Xico Almeida <francisco.almeida@outlook.com>
2022-05-24 19:01:31 -0300

    set buildkit as default for dockerfile building

--- docker.go
+++ docker.go
@@ -705,6 +705,7 @@ func (p *DockerProvider) BuildImage(ctx context.Context, img ImageBuildInfo) (st
 		Tags:        []string{repoTag},
 		Remove:      true,
 		ForceRemove: true,
+		Version:     types.BuilderBuildKit,
 	}
 
 	resp, err := p.client.ImageBuild(ctx, buildContext, buildOptions)
